a8bfa201010c44e626104e03c1b815abb25f1944
Noel Bor <NoelkBor@gmail.com>
2019-04-30 09:45:18 +0300

    add src/app/form-details/form-details.ts

--- src/app/form-details/form-details.component.ts
+++ src/app/form-details/form-details.component.ts
@@ -1,24 +1,24 @@
-// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-// import {Quote} from '../quote';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import {Quote} from '../quote';
 
-// @Component({
-//   selector: 'app-form-details',
-//   templateUrl: './form-details.component.html',
-//   styleUrls: ['./form-details.component.css']
-// })
-// export class FormDetailsComponent implements OnInit {
+@Component({
+  selector: 'app-form-details',
+  templateUrl: './form-details.component.html',
+  styleUrls: ['./form-details.component.css']
+})
+export class FormDetailsComponent implements OnInit {
 
-//   @Input() quote: Quote;
-//   @Output() isComplete = new EventEmitter<boolean>();
+  @Input() quote: Quote;
+  @Output() isComplete = new EventEmitter<boolean>();
 
-//   quoteDelete(complete: boolean) {
-//     this.isComplete.emit(complete);
-//   }
+  quoteDelete(complete: boolean) {
+    this.isComplete.emit(complete);
+  }
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {
-//   }
+  ngOnInit() {
+  }
 
-// }
+}
 
