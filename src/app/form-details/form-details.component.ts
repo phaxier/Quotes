35de01929c3db02b80cd103df6d00f65544c6b0e
Noel Bor <NoelkBor@gmail.com>
2019-04-30 09:38:41 +0300

    add src/app/form-details/

---
+++ src/app/form-details/form-details.component.ts
@@ -0,0 +1,24 @@
+// import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+// import {Quote} from '../quote';
+
+// @Component({
+//   selector: 'app-form-details',
+//   templateUrl: './form-details.component.html',
+//   styleUrls: ['./form-details.component.css']
+// })
+// export class FormDetailsComponent implements OnInit {
+
+//   @Input() quote: Quote;
+//   @Output() isComplete = new EventEmitter<boolean>();
+
+//   quoteDelete(complete: boolean) {
+//     this.isComplete.emit(complete);
+//   }
+
+//   constructor() { }
+
+//   ngOnInit() {
+//   }
+
+// }
+
