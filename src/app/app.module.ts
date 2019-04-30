440410b470b413389f34a06b2dc5e9c4a05eb4c2
Noel Bor <NoelkBor@gmail.com>
2019-04-30 10:06:32 +0300

    src/app/app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,31 @@
-// import { BrowserModule } from '@angular/platform-browser';
-// import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { QuoteComponent } from './quote/quote.component';
-// import { FormsComponent } from './forms/forms.component';
-// import { FormDetailsComponent } from './form-details/form-details.component';
-// import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { QuoteComponent } from './quote/quote.component';
+import { FormsComponent } from './forms/forms.component';
+import { FormDetailsComponent } from './form-details/form-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     QuoteComponent,
-//     FormsComponent,
-//     FormDetailsComponent,
+@NgModule({
+  declarations: [
+    AppComponent,
+    QuoteComponent,
+    FormsComponent,
+    FormDetailsComponent,
     
     
-//   ],
-//   imports: [
-//     BrowserModule,
-//     FormsModule,
-//     ReactiveFormsModule,
-//     AppRoutingModule,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
 
 
-//   ],
-//   providers: [],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
