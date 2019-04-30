d39a6674305fdf2a30aff9f0e46a1a06ca882a94
Noel Bor <NoelkBor@gmail.com>
2019-04-30 09:55:31 +0300

    src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,20 @@
-// import { Component } from '@angular/core';
-// import {Quote} from './quote';
+import { Component } from '@angular/core';
+import {Quote} from './quote';
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-// export class AppComponent {
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
   
-//   title = 'Quotes';
-//   /*quotes = [
-//     new Quote(1, 'Hello', 'Never'),
-//     new Quote(2, 'There', 'Give'),
-//     new Quote(3, 'you', 'Up'),
-//     new Quote(4, 'very', 'In'),
-//     new Quote(5, 'clever', 'Life'),
-//     new Quote(6, 'Guy', '..')
-//   ];*/
-// }
+  title = 'Quotes';
+  /*quotes = [
+    new Quote(1, 'Hello', 'Never'),
+    new Quote(2, 'There', 'Give'),
+    new Quote(3, 'you', 'Up'),
+    new Quote(4, 'very', 'In'),
+    new Quote(5, 'clever', 'Life'),
+    new Quote(6, 'Guy', '..')
+  ];*/
+}
