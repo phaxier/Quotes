c2b037ffb8e29fae1ce1f0010d53ddb6eb24bea6
Noel Bor <NoelkBor@gmail.com>
2019-04-30 09:40:51 +0300

    add src/app/form-details/form-details.ts

--- src/app/form-details/form-details.component.spec.ts
+++ src/app/form-details/form-details.component.spec.ts
@@ -1,25 +1,25 @@
-// import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { FormDetailsComponent } from './form-details.component';
+import { FormDetailsComponent } from './form-details.component';
 
-// describe('FormDetailsComponent', () => {
-//   let component: FormDetailsComponent;
-//   let fixture: ComponentFixture<FormDetailsComponent>;
+describe('FormDetailsComponent', () => {
+  let component: FormDetailsComponent;
+  let fixture: ComponentFixture<FormDetailsComponent>;
 
-//   beforeEach(async(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [ FormDetailsComponent ]
-//     })
-//     .compileComponents();
-//   }));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FormDetailsComponent ]
+    })
+    .compileComponents();
+  }));
 
-//   beforeEach(() => {
-//     fixture = TestBed.createComponent(FormDetailsComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FormDetailsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
